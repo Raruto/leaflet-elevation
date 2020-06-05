6aa4320d56d9a99ebfeb6d1203e30ae2d3cb9e06
Gérald Niel <gerald.niel@gegeweb.org>
2020-06-05 17:25:04 +0200

    insert after .minele, not before .download (fix error if no .download element)

--- libs/extended-ui.js
+++ libs/extended-ui.js
@@ -95,7 +95,7 @@ L.Control.Elevation.addInitHook(function() {
 	});
 
 	this.on("elechart_summary", function() {
-		this.summaryDiv.querySelector('.download').insertAdjacentHTML('beforebegin', '<span class="ascent"><span class="summarylabel">' + L._("Total Ascent: ") + '</span><span class="summaryvalue">' + Math.round(this.track_info.ascent) + '&nbsp;' +
+		this.summaryDiv.querySelector('.minele').insertAdjacentHTML('afterend', '<span class="ascent"><span class="summarylabel">' + L._("Total Ascent: ") + '</span><span class="summaryvalue">' + Math.round(this.track_info.ascent) + '&nbsp;' +
 			this._yLabel +
 			'</span></span>' + '<span class="descent"><span class="summarylabel">' + L._("Total Descent: ") + '</span><span class="summaryvalue">' + Math.round(this.track_info.descent) + '&nbsp;' + this._yLabel +
 			'</span></span>' + '<span class="minslope"><span class="summarylabel">' + L._("Min Slope: ") + '</span><span class="summaryvalue">' + this.track_info.slope_min + '&nbsp;' + '%' +
