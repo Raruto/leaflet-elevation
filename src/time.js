8ca3c3bb5933b8354d02fcfab5a5ce755b9585e6
Raruto <raruto@users.noreply.github.com>
2020-10-05 12:30:58 +0200

    renamed property names

--- src/time.js
+++ src/time.js
@@ -7,8 +7,12 @@ Elevation.addInitHook(function() {
 
 	if (!this.options.time) return;
 
-	if (!this.options.formatTime) {
-		this.options.formatTime = (time) => new Date(time).toLocaleString().replaceAll('/', '-').replaceAll(',', ' ');
+	if (!this.options.timeFormat) {
+		this.options.timeFormat = (time) => new Date(time).toLocaleString().replaceAll('/', '-').replaceAll(',', ' ');
+	} else if(this.options.timeFormat == 'time'){
+		this.options.timeFormat = (time) => new Date(time).toLocaleTimeString();
+	} else if(this.options.timeFormat == 'date'){
+		this.options.timeFormat = (time) => new Date(time).toLocaleDateString();
 	}
 
 	this.on("elechart_change", function(e) {
@@ -16,21 +20,21 @@ Elevation.addInitHook(function() {
 		let item = e.data;
 
 		if (chart._focuslabel) {
-			if (!!item.t) {
+			if (!!item.time) {
 				if (!chart._focuslabelTime || !chart._focuslabelTime.property('isConnected')) {
 					chart._focuslabelTime = chart._focuslabel.select('text')
 						.insert("svg:tspan", ".mouse-focus-label-x")
 						.attr("class", "mouse-focus-label-time")
 						.attr("dy", "1.5em");
 				}
-				chart._focuslabelTime.text(this.options.formatTime(item.t));
+				chart._focuslabelTime.text(this.options.timeFormat(item.time));
 			}
 		}
 	});
 
 	this.on('elepoint_added', function(e) {
 		if (e.point.meta) {
-			this._data[e.index].t = e.point.meta.time;
+			this._data[e.index].time = e.point.meta.time;
 		}
 	});
 
