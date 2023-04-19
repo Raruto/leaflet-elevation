88cfa1119986ed4c059bf5db643722d9fc482d35
Raruto <Raruto@users.noreply.github.com>
2023-04-19 19:17:32 +0200

    setup some `e2e` tests
    Start to handle: https://github.com/Raruto/leaflet-elevation/issues/233#issuecomment-1443929778

---
+++ e2e/leaflet-elevation.spec.ts
@@ -0,0 +1,26 @@
+// @ts-check
+import { test, expect, Route } from '@playwright/test';
+
+test.describe('leaflet-elevation.html', () => {
+
+  test.beforeEach(async ({ page, context }) => {
+    context.route(/.html$/, mock_cdn_urls);
+    await page.goto('/examples/leaflet-elevation.html');
+  });
+
+  test('has title', async ({ page }) => {
+    await expect(page).toHaveTitle(/leaflet-elevation\.js/);
+  });
+
+});
+
+/**
+ * Replace CDN URLs with locally developed files within Network response.
+ */
+async function mock_cdn_urls(route: Route) {
+  const response = await route.fetch();
+  let body = await response.text();
+  body = body.replace(new RegExp('https://unpkg.com/@raruto/leaflet-elevation@(.*?)/', 'g'), '../');
+  body = body.replace(new RegExp('@raruto/leaflet-elevation@(.*?)/', 'g'), '../');
+  route.fulfill({ response, body, headers: response.headers() });
+}
