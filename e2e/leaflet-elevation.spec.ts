5084824f3381f648c482f9913d29c551bd4363cc
Raruto <Raruto@users.noreply.github.com>
2023-04-20 09:02:19 +0200

    add test case for: https://github.com/Raruto/leaflet-elevation/issues/241

--- e2e/leaflet-elevation.spec.ts
+++ e2e/leaflet-elevation.spec.ts
@@ -1,22 +1,37 @@
 // @ts-check
 import { test, expect, Route } from '@playwright/test';
 
+/**
+ * This is a common global (window) variable declared within each `*.html` example
+ */
+declare const controlElevation: typeof import('../src/control.js').Elevation;
+
 test.describe('examples/leaflet-elevation.html', () => {
 
+  // Proxy network resources. 
   test.beforeEach(async ({ page, context }) => {
+    // page.on('console', (msg) => { console.log(msg);});
     context.route(/.html$/, mock_cdn_urls);
     await page.goto('/examples/leaflet-elevation.html');
   });
 
-  test('has title', async ({ page }) => {
+  // Wait until page is fully loaded.
+  test.afterEach(async ({ page, context }) => {
     await expect(page).toHaveTitle(/leaflet-elevation\.js/);
+  });
+
+  test('eledata_loaded', async ({ page }) => {
+    const gpx = await page.evaluate<any>(() => new Promise(resolve => {
+      controlElevation.on('eledata_loaded', (gpx) => resolve(gpx));
+    }));
+    expect(gpx.name).toBe('via-emilia.gpx');
+    expect(gpx.layer).toBeDefined();
+    expect(gpx.track_info).toBeDefined();
+    expect(gpx.track_info.distance).not.toBeNaN();
   });
 
 });
 
-/**
- * Replace CDN URLs with locally developed files within Network response.
- */
 async function mock_cdn_urls(route: Route) {
   const response = await route.fetch();
   let body = await response.text();
