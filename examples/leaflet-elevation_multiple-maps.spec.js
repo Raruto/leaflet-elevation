ea73530b3509f66406fa97c7a623e715573a75b9
Raruto <Raruto@users.noreply.github.com>
2023-04-23 16:42:18 +0200

    Update leaflet-elevation_multiple-maps.spec.js

--- examples/leaflet-elevation_multiple-maps.spec.js
+++ examples/leaflet-elevation_multiple-maps.spec.js
@@ -7,10 +7,15 @@ import { suite } from '../test/setup/http_server.js';
 
 const test = suite('examples/leaflet-elevation_multiple-maps.html');
 
+/**
+ * @see https://github.com/Raruto/leaflet-elevation/issues/238#issuecomment-1493717835
+ * @see https://github.com/Raruto/leaflet-elevation/issues/238#issuecomment-1493826767
+ */
 test('multiple-maps', async ({ page }) => {
-    const default_margins = await page.evaluate(() => new Promise(resolve => {
-        resolve(L.Control.Elevation.prototype.options.margins);
+    const { charts, default_margins } = await page.evaluate(() => new Promise(resolve => {
+        resolve({ charts, default_margins: L.Control.Elevation.prototype.options.margins });
     }));
+    charts.forEach(chart => { assert.is(chart.options.legend, true); });
     assert.snapshot(
         JSON.stringify(default_margins),
         JSON.stringify({ top: 30, right: 30, bottom: 30, left: 40 })
