Raruto/leaflet-elevation

commit bfafa910fc0cfd19364bc7d6f1cba8b8770a43dc

Prevent shallow copy of default options (multiple charts) (#240)
* Prevent shallow copy of default options (multiple charts)

closes: https://github.com/Raruto/leaflet-elevation/issues/238

* Update control.js

src/control.js
@@ -174,7 +174,10 @@ export const Elevation = L.Control.Elevation = L.Control.extend({
 	 */
 	initialize: function(opts) {
 
-		opts = L.setOptions(this, opts);
+		// opts = L.setOptions(this, opts);
+
+		// Fixes: https://github.com/Raruto/leaflet-elevation/pull/240
+		opts = L.setOptions(this, L.extend({}, structuredClone(Options), opts)); // "deep copy" nested objects (multiple charts)
 
 		this._data           = [];
 		this._layers         = L.featureGroup();

commit f6e5e4423073a59a74f6552e9693ba353a8fa947

Replace `structuredClone` with a custom `cloneDeep` function
Fixes a regression introduced by: https://github.com/Raruto/leaflet-elevation/pull/240

Closes: https://github.com/Raruto/leaflet-elevation/issues/255

examples/leaflet-elevation_multiple-maps.html
@@ -91,6 +91,7 @@
 
 		for (const id of ['map-1', 'map-2', 'map-3']) {
 			opts.elevationControl.options.detached = ['map-3'].includes(id);
+			opts.elevationControl.options.marker   = ['map-3'].includes(id) ? 'position-marker' : 'elevation-line';
 
 			let map = L.map(id, opts.map);
 

src/control.js
@@ -175,7 +175,7 @@ export const Elevation = L.Control.Elevation = L.Control.extend({
 		// opts = L.setOptions(this, opts);
 
 		// Fixes: https://github.com/Raruto/leaflet-elevation/pull/240
-		opts = L.setOptions(this, L.extend({}, structuredClone(Options), opts)); // "deep copy" nested objects (multiple charts)
+		opts = L.setOptions(this, L.extend({}, _.cloneDeep(Options), opts)); // "deep copy" nested objects (multiple charts)
 
 		this._data           = [];
 		this._layers         = L.featureGroup();

src/utils.js
@@ -161,3 +161,49 @@ export const clamp     = (val, range)           => range ? (val < range[0] ? ran
  * Limit a delta difference between two values
  */
 export const wrapDelta = (curr, prev, deltaMax) => Math.abs(curr - prev) > deltaMax ? prev + deltaMax * Math.sign(curr - prev) : curr;
+
+/**
+ * A deep copy implementation that takes care of correct prototype chain and cycles, references
+ * 
+ * @see https://web.dev/structured-clone/#features-and-limitations
+ */
+export function cloneDeep(o, skipProps) {
+	const origins = [], clones = [];                            // cycle and reference detection
+	skipProps = skipProps || [];
+	function deepCopy(o) {
+		switch(!o || typeof o) {
+			case 'object':
+				const origin = origins.indexOf(o);
+				if (-1 !== origin) return clones[origin];             // retrieve copied object from cache
+				const copy = Object.create(Object.getPrototypeOf(o));
+				origins.push(o);                                      // save copy in cache before looping
+				clones.push(copy);                                    // props, in case there is a cycle
+				Object
+					.getOwnPropertyNames(o)
+					.forEach(function (prop) {
+						const propdesc = Object.getOwnPropertyDescriptor(o, prop);
+						Object.defineProperty(
+							copy,
+							prop,
+							propdesc.get || propdesc.set
+								? propdesc                                    // just copy accessor properties
+								: {                                           // deep copy data properties
+									writable:     propdesc.writable,
+									configurable: propdesc.configurable,
+									enumerable:   propdesc.enumerable,
+									value:        skipProps.includes(prop) ? propdesc.value : deepCopy(propdesc.value),
+								}
+						);
+					});
+				return copy;
+			case 'function':
+			case 'symbol':
+				console.warn('cloneDeep: ' + typeof o + 's not fully supported:', o);
+			case true:
+				// null, undefined or falsy primitive
+			default:
+				return o;
+		}
+	}
+	return deepCopy(o);
+}