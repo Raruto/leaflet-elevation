0acd3e1c0845485fc5a59df549f57523774863a9
Raruto <Raruto@users.noreply.github.com>
2023-06-13 21:29:00 +0200

    test case for: https://github.com/Raruto/leaflet-elevation/issues/259

--- examples/leaflet-elevation_almost-over.spec.js
+++ examples/leaflet-elevation_almost-over.spec.js
@@ -29,6 +29,7 @@ test('almostOver', async ({ page }) => {
     const step_2 = await load_trace(page, './via-aurelia.gpx');
     assert.is(step_2.gpx.name, 'via-aurelia.gpx');
     assert.not.type(step_2.gpx.layer, 'undefined');
+
     assert.snapshot(
       JSON.stringify(step_2.gpx.track_info),
       JSON.stringify({
@@ -71,6 +72,17 @@ test('almostOver', async ({ page }) => {
     assert.equal(step_2.over_layers, 1);
     assert.equal(step_3.over_layers, 9);
 
+    /**
+     * List of points where altitude == 0
+     * 
+     * <ele>0</ele> values shouldn't be interpreted as an empty ("false" or "undefined") value.
+     * 
+     * @see https://github.com/Raruto/leaflet-elevation/issues/259
+     */
+    [1029, 1030, 2048, 3747, 3843, 3845, 3891, 3965, 3966, 4057, 4058, 4072, 4262, 4263, 4264, 4269, 11284].forEach(idx => {
+      assert.is(step_2.data[idx].z, 0);
+    });
+
 }, 15000);
 
 test.run();
@@ -90,6 +102,7 @@ async function load_trace(page, trace_url) {
             option: ctrl.options.almostOver,
             over_layers: map.almostOver._layers.length,
             map_layers: Object.keys(map._layers).length,
+            data: ctrl._data,
           }));
       });
       ctrl.load(trace_url);
