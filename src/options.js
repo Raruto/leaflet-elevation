50f33f3032ed6653faa50b7f5cf74744bad48e44
Raruto <Raruto@users.noreply.github.com>
2023-11-02 16:25:39 +0100

    remove default handler

--- src/options.js
+++ src/options.js
@@ -18,7 +18,7 @@ export var Options = {
 	followMarker: true,
 	imperial: false,
 	legend: true,
-	handlers: ["Distance", "Time", "Altitude", "Slope", "Speed", "Acceleration", 'Temperature'],
+	handlers: ["Distance", "Time", "Altitude", "Slope", "Speed", "Acceleration"],
 	hotline: 'elevation',
 	marker: 'elevation-line',
 	markerIcon: L.divIcon({
